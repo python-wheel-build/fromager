dd2bcb9ee15697538f644ead2cb796938d178413
Mark McLoughlin <markmc@redhat.com>
2024-03-20 14:11:42 +0000

    Move system setup comment to setup.sh

--- mirror-sdists.sh
+++ mirror-sdists.sh
@@ -35,23 +35,6 @@ setup
 
 pip install -U python-pypi-mirror toml pyproject_hooks packaging wheel
 
-# cmake needed, otherwise:
-# Building wheels for collected packages: patchelf, ninja
-# ...
-# Building wheel for patchelf (pyproject.toml) did not run successfully.
-# ...
-# Problem with the CMake installation, aborting build. CMake executable is cmake
-
-# autoconf/automake needed, otherwise e.g.:
-# [ 44%] Performing patch step for 'build_patchelf'
-# ./bootstrap.sh: line 2: autoreconf: command not found
-
-# rust/cargo needed, otherwise:
-# Building wheels for collected packages: maturin
-# error: can't find Rust compiler
-
-# $ sudo dnf install cmake autoconf automake rust cargo
-
 add_to_build_order() {
   local type="$1"; shift
   local req="$1"; shift
--- setup.sh
+++ setup.sh
@@ -2,4 +2,19 @@
 
 set -e -o pipefail
 
+# cmake needed, otherwise:
+# Building wheels for collected packages: patchelf, ninja
+# ...
+# Building wheel for patchelf (pyproject.toml) did not run successfully.
+# ...
+# Problem with the CMake installation, aborting build. CMake executable is cmake
+
+# autoconf/automake needed, otherwise e.g.:
+# [ 44%] Performing patch step for 'build_patchelf'
+# ./bootstrap.sh: line 2: autoreconf: command not found
+
+# rust/cargo needed, otherwise:
+# Building wheels for collected packages: maturin
+# error: can't find Rust compiler
+
 sudo dnf install python3 python3-devel rust cargo cmake autoconf automake
