266ac6614e6a98f93ab9e677bbfa3c0b79af5bd7
Mark McLoughlin <markmc@redhat.com>
2024-03-20 17:39:28 +0000

    Explicitly handle lack of build-system in pyproject.toml
    Related to #4
    
    PEP517 says:
    
    > If the pyproject.toml file is absent, or the build-backend key is missing,
    > the source tree is not using this specification, and tools should revert
    > to the legacy behaviour of running setup.py (either directly, or by
    > implicitly invoking the setuptools.build_meta:__legacy__ backend).

--- extract-requires.py
+++ extract-requires.py
@@ -39,6 +39,18 @@ def logging_subprocess_runner(cmd, cwd=None, extra_environ=None):
         raise subprocess.CalledProcessError(completed.returncode, cmd, output)
 
 
+def get_build_backend_hook_caller(pyproject_toml):
+    if (not 'build-system' in pyproject_toml or
+        not 'build-backend' in pyproject_toml['build-system']):
+        return None
+    return pyproject_hooks.BuildBackendHookCaller(
+        source_dir=".",
+        build_backend=pyproject_toml['build-system']['build-backend'],
+        backend_path=pyproject_toml['build-system'].get('backend-path', None),
+        runner=logging_subprocess_runner,
+    )
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--build-system", action=argparse.BooleanOptionalAction)
@@ -46,16 +58,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     pyproject_toml = toml.loads(sys.stdin.read())
+    hook_caller = get_build_backend_hook_caller(pyproject_toml)
+    if hook_caller is None:
+        raise Exception('No build-system in pyproject.toml - FIXME implement the legacy behaviour of running setup.py')
 
     requires = []
     if not (args.build_system or args.build_backend):
         requires.extend(pyproject_toml.get('project', {}).get('dependencies', []))
-        hook_caller = pyproject_hooks.BuildBackendHookCaller(
-            source_dir=".",
-            build_backend=pyproject_toml.get('build-system', {}).get('build-backend', ''),
-            backend_path=pyproject_toml.get('build-system', {}).get('backend-path', None),
-            runner=logging_subprocess_runner,
-        )
+
         metadata_path = hook_caller.prepare_metadata_for_build_wheel("./")
 
         with open(os.path.join(metadata_path, "METADATA"), "r") as f:
@@ -66,14 +76,7 @@ if __name__ == "__main__":
     elif args.build_system:
         requires.extend(pyproject_toml.get('build-system', {}).get('requires', []))
     elif args.build_backend:
-        if 'build-backend' in pyproject_toml.get('build-system', {}):
-            hook_caller = pyproject_hooks.BuildBackendHookCaller(
-                source_dir=".",
-                build_backend=pyproject_toml.get('build-system', {}).get('build-backend', ''),
-                backend_path=pyproject_toml.get('build-system', {}).get('backend-path', None),
-                runner=logging_subprocess_runner,
-            )
-            requires.extend(hook_caller.get_requires_for_build_wheel())
+        requires.extend(hook_caller.get_requires_for_build_wheel())
 
     for req in requires:
         print(f"{req}")
