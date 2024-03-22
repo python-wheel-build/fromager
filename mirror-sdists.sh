b6ae858654851cd9221eac838bc6fbf59a878aaf
Doug Hellmann <doug@doughellmann.com>
2024-03-22 14:47:30 -0400

    mirror-sdists: build wheels as we process packages
    Build the wheels for packages as they are mirrored so that the local
    wheel server has all of the dependencies needed to install things.

--- mirror-sdists.sh
+++ mirror-sdists.sh
@@ -20,6 +20,7 @@ TOPLEVEL="${1:-langchain}"
 VENV=$WORKDIR/venv
 PYTHON=python3.9
 
+WHEELS_REPO=$(realpath $(pwd)/wheels-repo)
 SDISTS_REPO=$(realpath $(pwd)/sdists-repo)
 
 setup() {
@@ -48,6 +49,17 @@ add_to_build_order() {
   local why="$1"; shift
   jq --argjson obj "{\"type\":\"${type}\",\"req\":\"${req//\"/\'}\",\"why\":\"${why}\"}" '. += [$obj]' ${SDISTS_REPO}/build-order.json > tmp.$$.json && mv tmp.$$.json ${SDISTS_REPO}/build-order.json
 }
+
+update_mirror() {
+  pypi-mirror create -d "${WHEELS_REPO}/downloads" -m "${WHEELS_REPO}/simple"
+}
+
+build_wheel() {
+  local sdist_dir="$1"; shift
+  local dest_dir="$1"; shift
+
+  (cd "${sdist_dir}" && python -m build --wheel . && mv dist/*.whl "${WHEELS_REPO}/downloads/")
+  update_mirror
 }
 
 download_sdist() {
@@ -58,6 +70,17 @@ download_sdist() {
 get_downloaded_sdist() {
     local input=$1
     grep Saved $input | cut -d ' ' -f 2-
+}
+
+safe_install() {
+    local req="$1"; shift
+
+    pip -vvv install \
+        --upgrade \
+        --disable-pip-version-check \
+        --only-binary :all: \
+        --index-url "${WHEEL_SERVER_URL}" \
+        "${req}"
 }
 
 collect_build_requires() {
@@ -94,7 +117,7 @@ collect_build_requires() {
         # We may need these dependencies installed in order to run build hooks
         # Example: frozenlist build-system.requires includes expandvars because
         # it is used by the packaging/pep517_backend/ build backend
-        pip install -U "${req_iter}"
+        safe_install "${req_iter}"
       fi
   done
 
@@ -113,7 +136,7 @@ collect_build_requires() {
       # Build backends are often used to package themselves, so in
       # order to determine their dependencies they may need to be
       # installed.
-      pip install -U "${req_iter}"
+      safe_install "${req_iter}"
     fi
   done
 
@@ -130,7 +153,25 @@ collect_build_requires() {
       add_to_build_order "dependency" "${req_iter}" "${why}"
     fi
   done
+
+  # Build the wheel for this package after handling all of the dependencies
+  build_wheel "${extract_dir}" "${WHEELS_REPO}/downloads"
 }
+
+stop_wheel_server() {
+    kill ${HTTP_SERVER_PID}
+}
+start_wheel_server() {
+    update_mirror
+    $PYTHON -m http.server --directory "${WHEELS_REPO}" 9090 &
+    HTTP_SERVER_PID=$!
+    WHEEL_SERVER_URL="http://localhost:9090/simple"
+}
+
+on_exit() {
+    stop_wheel_server
+}
+trap on_exit EXIT SIGINT SIGTERM
 
 setup
 
@@ -139,6 +180,7 @@ echo -n "[]" > ${SDISTS_REPO}/build-order.json
 
 rm -rf "${WHEELS_REPO}"
 mkdir -p "${WHEELS_REPO}/downloads"
+start_wheel_server
 
 download_sdist "${TOPLEVEL}" | tee $WORKDIR/toplevel-download.log
 collect_build_requires $(get_downloaded_sdist $WORKDIR/toplevel-download.log) ""
