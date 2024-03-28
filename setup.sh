06d00eb8c180c681b1e9401cc955ff668dcd61c9
Doug Hellmann <doug@doughellmann.com>
2024-03-28 17:56:33 -0400

    create a containerfile that can run the test script
    Move the contents of setup.sh into the containerfile and reuse it so
    we only have one list of dependencies to maintain.

--- setup.sh
+++ setup.sh
@@ -2,34 +2,5 @@
 
 set -e -o pipefail
 
-# cmake needed, otherwise:
-# Building wheels for collected packages: patchelf, ninja
-# ...
-# Building wheel for patchelf (pyproject.toml) did not run successfully.
-# ...
-# Problem with the CMake installation, aborting build. CMake executable is cmake
-
-# autoconf/automake needed, otherwise e.g.:
-# [ 44%] Performing patch step for 'build_patchelf'
-# ./bootstrap.sh: line 2: autoreconf: command not found
-
-# rust/cargo needed, otherwise:
-# Building wheels for collected packages: maturin
-# error: can't find Rust compiler
-
-sudo dnf install -y python3 python3-devel cmake autoconf automake patch
-
-# Needed for setuptools-rust and maturin build
-sudo dnf install -y rust cargo
-
-# Needed for cffi build
-sudo dnf install -y libffi libffi-devel
-
-# Needed for cryptography build
-sudo dnf install -y openssl-devel
-
-# Needed for pillow build
-sudo dnf install -y zlib-devel libjpeg-devel
-
-# Needed for 3.12 builds (on CentOS Stream 9)
-sudo dnf install -y python3.12-devel
+# Look for individual packages, but not the dev tool groups.
+grep '^RUN dnf -y install' Containerfile | sed 's/RUN /sudo /g' | sh -x
