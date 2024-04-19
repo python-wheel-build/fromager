236d2ef3b2c4ebda5bfdc7a1868d5eeb5c9a452e
Doug Hellmann <doug@doughellmann.com>
2024-04-19 16:45:54 -0400

    configure linters to look at all of the code

--- tests/test_external_commands.py
+++ tests/test_external_commands.py
@@ -1,9 +1,7 @@
-import subprocess
-
 from mirror_builder import external_commands
 
 
 def test_external_commands_environ():
-    env = { "BLAH": "test" }
-    output = external_commands.run([ "sh", "-c", "echo $BLAH" ], extra_environ=env)
+    env = {"BLAH": "test"}
+    output = external_commands.run(["sh", "-c", "echo $BLAH"], extra_environ=env)
     assert "test\n" == output
