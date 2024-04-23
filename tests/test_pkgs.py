3d5c740d16906523bd8ae2107b0653702ac10a45
Mark McLoughlin <markmc@redhat.com>
2024-04-23 04:56:32 -0400

    Add patch to build fbgemm with -Wno-maybe-uninitialized

--- tests/test_pkgs.py
+++ tests/test_pkgs.py
@@ -29,8 +29,9 @@ def test_nodist():
 @pytest.mark.parametrize('dir_name,expected_patches', [
     ('clarifai-10.2.1', ['clarifai-10.2.1-fix-sdist.patch']),
     ('ninja-1.11.1.1', ['ninja-1.11.1.1-wrap-system-ninja.patch']),
-    ('pytorch-v2.2.2', ['pytorch-v2.2.2-0001-remove-cmake-build-requirement.patch',
-                        'pytorch-v2.2.2-002-dist-info-no-run-build-deps.patch']),
+    ('pytorch-v2.2.2', ['pytorch-v2.2.2-001-remove-cmake-build-requirement.patch',
+                        'pytorch-v2.2.2-002-dist-info-no-run-build-deps.patch',
+                        'pytorch-v2.2.2-003-fbgemm-no-maybe-uninitialized.patch']),
 ])
 def test_patches_for_source_dir(dir_name, expected_patches):
     patches = list(pkgs.patches_for_source_dir(dir_name))
