bf48c3758a4893a958a32c636aec54ddad588ac4
Mark McLoughlin <markmc@redhat.com>
2024-04-25 11:16:00 +0100

    test_sdist: isolate from PyPI
    Mock the resolve_sdist() call so we don't depend on PyPI during
    testing.

--- tests/test_sdist.py
+++ tests/test_sdist.py
@@ -1,9 +1,19 @@
+from unittest.mock import patch
+
 from packaging.requirements import Requirement
+from packaging.version import Version
 
 from mirror_builder import sdist
 
 
-def test_missing_dependency_format():
+@patch('mirror_builder.sources.resolve_sdist')
+def test_missing_dependency_format(resolve_sdist):
+    resolutions = {
+        'flit_core': '3.9.0',
+        'setuptools': '69.5.1',
+    }
+    resolve_sdist.side_effect = lambda req: ('', Version(resolutions[req.name]))
+
     req = Requirement('setuptools>=40.8.0')
     other_reqs = [
         Requirement('flit_core'),
@@ -14,6 +24,6 @@ def test_missing_dependency_format():
     # Ensure we report the thing we're actually missing
     assert 'Failed to install test dependency setuptools>=40.8.0. ' in s
     # Ensure we report what version we expected of that thing
-    assert 'setuptools>=40.8.0 -> ' in s
+    assert 'setuptools>=40.8.0 -> 69.5.1' in s
     # Ensure we report what version we expect of all of the other dependencies
-    assert 'flit_core -> ' in s
+    assert 'flit_core -> 3.9.0' in s
