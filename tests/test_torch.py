python-wheel-build/fromager

commit 9e7fb86ae41d557c28d6ac680978cab4462dd4a7

pass a url for finding source distribution files
In restricted builds we will only download source packages that are
already available on the index server.

Addresses #112

mirror_builder/sources.py
@@ -14,18 +14,18 @@ from . import pkgs, resolver
 logger = logging.getLogger(__name__)
 
 
-def download_source(ctx, req):
+def download_source(ctx, req, sdist_server_url='https://pypi.org/simple'):
     downloader = pkgs.find_override_method(req.name, 'download_source')
     if not downloader:
         downloader = _default_download_source
-    source_filename, version = downloader(ctx, req)
+    source_filename, version = downloader(ctx, req, sdist_server_url)
     return (source_filename, version)
 
 
-def resolve_sdist(req):
+def resolve_sdist(req, sdist_server_url):
     "Return URL to source and its version."
     # Create the (reusable) resolver. Limit to sdists.
-    provider = resolver.PyPIProvider(only_sdists=True)
+    provider = resolver.PyPIProvider(only_sdists=True, sdist_server_url=sdist_server_url)
     reporter = resolvelib.BaseReporter()
     rslvr = resolvelib.Resolver(provider, reporter)
 
@@ -41,11 +41,12 @@ def resolve_sdist(req):
 
     for name, candidate in result.mapping.items():
         return (candidate.url, candidate.version)
+    return (None, None)
 
 
-def _default_download_source(ctx, req):
+def _default_download_source(ctx, req, sdist_server_url):
     "Download the requirement and return the name of the output path."
-    url, version = resolve_sdist(req)
+    url, version = resolve_sdist(req, sdist_server_url)
     source_filename = download_url(ctx.sdists_downloads, url)
     logger.info('have source for %s version %s in %s', req, version, source_filename)
     return (source_filename, version)

Containerfile.e2e-one-wheel
@@ -9,6 +9,7 @@ ENV VERSION=${VERSION}
 
 # Where do we find dependencies?
 ARG WHEEL_SERVER_URL
+ARG SDIST_SERVER_URL
 
 ENV VIRTUAL_ENV=/venv
 ENV PATH=$VIRTUAL_ENV/bin:$PATH
@@ -21,6 +22,7 @@ RUN python3 -m mirror_builder -v \
     --work-dir /work-dir \
     --sdists-repo /sdists-repo \
     --wheels-repo /wheels-repo \
+    --sdist-server-url ${SDIST_SERVER_URL} \
     download-source-archive ${DIST} ${VERSION}
 
 # Prepare the source dir for building

build_wheel.sh
@@ -78,7 +78,8 @@ build_wheel_isolated() {
          --tag "e2e-build-$dist" \
          --build-arg="DIST=$dist" \
          --build-arg="VERSION=$version" \
-         --build-arg="WHEEL_SERVER_URL=$WHEEL_SERVER_URL"
+         --build-arg="WHEEL_SERVER_URL=$WHEEL_SERVER_URL" \
+         --build-arg="SDIST_SERVER_URL=$SDIST_SERVER_URL"
 
   # Run the image to build the wheel.
   podman run -it \
@@ -114,6 +115,7 @@ build_wheel() {
           --work-dir "$WORKDIR" \
           --sdists-repo sdists-repo \
           --wheels-repo wheels-repo \
+          --sdist-server-url "$SDIST_SERVER_URL" \
           download-source-archive "${DIST}" "${VERSION}"
 
   # Prepare the source dir for building

common.sh
@@ -16,6 +16,9 @@ export WORKDIR="${WORKDIR:-${DEFAULT_WORKDIR}}"
 # repo.
 export TOOL_SERVER_URL=https://pyai.fedorainfracloud.org/internal/tools/+simple/
 
+# Index server where we have sdists for the software we are building.
+export SDIST_SERVER_URL=https://pyai.fedorainfracloud.org/experimental/sources/+simple/
+
 # Set a default URL until we have our private one running.
 export WHEEL_SERVER_URL=${WHEEL_SERVER_URL:-https://pypi.org/simple}
 

mirror_builder/__main__.py
@@ -39,6 +39,7 @@ def main():
     parser_download.set_defaults(func=do_download_source_archive)
     parser_download.add_argument('dist_name')
     parser_download.add_argument('dist_version')
+    parser.add_argument('--sdist-server-url')
 
     parser_prepare_source = subparsers.add_parser('prepare-source')
     parser_prepare_source.set_defaults(func=do_prepare_source)
@@ -111,7 +112,7 @@ def do_bootstrap(args, ctx):
 def do_download_source_archive(args, ctx):
     req = Requirement(f'{args.dist_name}=={args.dist_version}')
     logger.info('downloading source archive for %s', req)
-    filename, _ = sources.download_source(ctx, req)
+    filename, _ = sources.download_source(ctx, req, args.sdist_server_url)
     print(filename)
 
 

mirror_builder/resolver.py
@@ -76,11 +76,11 @@ class Candidate:
         return self._dependencies
 
 
-def get_project_from_pypi(project, extras):
+def get_project_from_pypi(project, extras, sdist_server_url):
     """Return candidates created from the project name and extras."""
-    logger.debug('get available versions of project %s', project)
-    url = "https://pypi.org/simple/{}".format(project)
-    data = requests.get(url).content
+    simple_index_url = sdist_server_url.rstrip('/') + '/' + project + '/'
+    logger.debug('get available versions of project %s from %s', project, simple_index_url)
+    data = requests.get(simple_index_url).content
     doc = html5lib.parse(data, namespaceHTMLElements=False)
     for i in doc.findall(".//a"):
         url = i.attrib["href"]
@@ -138,9 +138,10 @@ def get_metadata_for_wheel(url):
 
 
 class PyPIProvider(ExtrasProvider):
-    def __init__(self, only_sdists=False):
+    def __init__(self, only_sdists=False, sdist_server_url='https://pypi.org/simple/'):
         super().__init__()
         self.only_sdists = only_sdists
+        self.sdist_server_url = sdist_server_url
 
     def identify(self, requirement_or_candidate):
         return canonicalize_name(requirement_or_candidate.name)
@@ -164,7 +165,7 @@ class PyPIProvider(ExtrasProvider):
         # treat candidates as immutable once created.
         candidates = (
             candidate
-            for candidate in get_project_from_pypi(identifier, set())
+            for candidate in get_project_from_pypi(identifier, set(), self.sdist_server_url)
             if (candidate.is_sdist or not self.only_sdists)
             and candidate.version not in bad_versions
             and all(candidate.version in r.specifier for r in requirements)

commit b3d87fe9f12d753d9a836a0c23fb19e967004a71

Fix torch.download_source()
commit 9e7fb86ae broke this by adding a new parameter not reflected
in the override.

First, chain to sources.default_download_source() when we're downloading
the cached copy - that should work as normal.

Second, use sources.resolve_sdist() with a new only_sdists parameter
rather than copying the implementation. This also makes it easier to
intercept the resolution during unit testing.

Finally, add a unit test so this doesn't break in future.

mirror_builder/pkgs/overrides/torch.py
@@ -1,8 +1,6 @@
 import logging
 
-import resolvelib
-
-from mirror_builder import resolver, sources
+from mirror_builder import sources
 
 logger = logging.getLogger(__name__)
 
@@ -10,29 +8,19 @@ logger = logging.getLogger(__name__)
 # Note that we're downloading a PyTorch release tarball that is not strictly an
 # sdist. Instead it's an archive of the git repo produced by the create_release
 # workflow which (crucially) includes the third_party/ submodules.
-def download_source(ctx, req):
-    provider = resolver.PyPIProvider(only_sdists=False)
-    reporter = resolvelib.BaseReporter()
-    rslvr = resolvelib.Resolver(provider, reporter)
+def download_source(ctx, req, sdist_server_url):
+    # Downloading source from upstream is the special case
+    if "pypi.org" not in sdist_server_url:
+        return sources.default_download_source(ctx, req, sdist_server_url)
 
-    logger.debug("resolving requirement %s", req)
-    try:
-        result = rslvr.resolve([req])
-    except (resolvelib.InconsistentCandidate,
-            resolvelib.RequirementsConflicted,
-            resolvelib.ResolutionImpossible) as err:
-        logger.warning(f'could not resolve {req}: {err}')
-        raise
-
-    for _, candidate in result.mapping.items():
-        logger.info(f"resolved {req} to {candidate.version}")
-        source_filename = sources.download_url(
-            ctx.sdists_downloads,
-            _get_pytorch_release_tarball_url(candidate.version),
-        )
-        logger.info('have source for %s version %s in %s',
-                    req, candidate.version, source_filename)
-        return (source_filename, candidate.version)
+    _, version = sources.resolve_sdist(req, sdist_server_url, only_sdists=False)
+    logger.info(f"resolved {req} to {version}")
+    source_filename = sources.download_url(
+        ctx.sdists_downloads,
+        _get_pytorch_release_tarball_url(version),
+    )
+    logger.info('have source for %s version %s in %s', req, version, source_filename)
+    return source_filename, version
 
 
 def _get_pytorch_release_tarball_url(version):

mirror_builder/sources.py
@@ -17,15 +17,15 @@ logger = logging.getLogger(__name__)
 def download_source(ctx, req, sdist_server_url='https://pypi.org/simple'):
     downloader = pkgs.find_override_method(req.name, 'download_source')
     if not downloader:
-        downloader = _default_download_source
+        downloader = default_download_source
     source_filename, version = downloader(ctx, req, sdist_server_url)
     return (source_filename, version)
 
 
-def resolve_sdist(req, sdist_server_url):
+def resolve_sdist(req, sdist_server_url, only_sdists=True):
     "Return URL to source and its version."
     # Create the (reusable) resolver. Limit to sdists.
-    provider = resolver.PyPIProvider(only_sdists=True, sdist_server_url=sdist_server_url)
+    provider = resolver.PyPIProvider(only_sdists=only_sdists, sdist_server_url=sdist_server_url)
     reporter = resolvelib.BaseReporter()
     rslvr = resolvelib.Resolver(provider, reporter)
 
@@ -44,7 +44,7 @@ def resolve_sdist(req, sdist_server_url):
     return (None, None)
 
 
-def _default_download_source(ctx, req, sdist_server_url):
+def default_download_source(ctx, req, sdist_server_url):
     "Download the requirement and return the name of the output path."
     url, version = resolve_sdist(req, sdist_server_url)
     source_filename = download_url(ctx.sdists_downloads, url)

tests/test_torch.py
@@ -1,0 +1,58 @@
+import os.path
+from unittest.mock import patch
+
+from packaging.requirements import Requirement
+from packaging.version import Version
+
+from mirror_builder.pkgs.overrides import torch
+
+
+@patch('mirror_builder.sources.download_url')
+@patch('mirror_builder.sources.resolve_sdist')
+def test_torch_download_source_from_upstream(resolve_sdist, download_url, tmp_context):
+    def mock_resolve_sdist(req, sdist_server_url, only_sdists=True):
+        # Uses custom resolve_sdist() for sources from upstream
+        assert not only_sdists
+        resolutions = {
+            'torch': '2.2.1',
+        }
+        return '', Version(resolutions[req.name])
+    resolve_sdist.side_effect = mock_resolve_sdist
+
+    def mock_download_url(destination_dir, url):
+        assert url == 'https://github.com/pytorch/pytorch/releases/download/v2.2.1/pytorch-v2.2.1.tar.gz'
+        return 'pytorch-v2.2.1.tar.gz'
+    download_url.side_effect = mock_download_url
+
+    req = Requirement('torch')
+    sdist_server_url = 'https://pypi.org/simple'
+    f, v = torch.download_source(tmp_context, req, sdist_server_url)
+
+    assert str(v) == '2.2.1'
+    assert os.path.basename(f) == 'pytorch-v2.2.1.tar.gz'
+
+
+@patch('mirror_builder.sources.download_url')
+@patch('mirror_builder.sources.resolve_sdist')
+def test_torch_download_source_from_cache(resolve_sdist, download_url, tmp_context):
+    def mock_resolve_sdist(req, sdist_server_url, only_sdists=True):
+        assert sdist_server_url == 'not_upstream'
+        # Uses regular resolve_sdist() for cached sources
+        assert only_sdists
+        resolutions = {
+            'torch': '2.2.1',
+        }
+        return 'not_upstream_url', Version(resolutions[req.name])
+    resolve_sdist.side_effect = mock_resolve_sdist
+
+    def mock_download_url(destination_dir, url):
+        assert url == 'not_upstream_url'
+        return 'pytorch-v2.2.1.tar.gz'
+    download_url.side_effect = mock_download_url
+
+    req = Requirement('torch')
+    sdist_server_url = 'not_upstream'
+    f, v = torch.download_source(tmp_context, req, sdist_server_url)
+
+    assert str(v) == '2.2.1'
+    assert os.path.basename(f) == 'pytorch-v2.2.1.tar.gz'