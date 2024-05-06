4056df82d4dbb658dcd0dbe0897713fe45dfa349
Doug Hellmann <dhellmann@redhat.com>
2024-05-06 15:38:29 -0400

    cuda: script and container to build anything with cuda enabled

---
+++ cuda/build_cuda.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# -*- indent-tabs-mode: nil; tab-width: 2; sh-indentation: 2; -*-
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TOPDIR="$( cd "${SCRIPTDIR}/.." && pwd )"
+# shellcheck disable=SC1091
+source "$TOPDIR/common.sh"
+
+outside_of_container() {
+    mkdir -p "$WORKDIR"
+    mkdir -p sdists-repo
+    mkdir -p wheels-repo
+    mkdir -p build-logs
+
+    # Redirect stdout/stderr to logfile
+    logfile="$WORKDIR/build-cuda.log"
+    exec > >(tee "$logfile") 2>&1
+
+    image_tag="cuda-ubi9-builder"
+
+    podman build \
+           --tag "$image_tag" \
+           -f "$SCRIPTDIR/Containerfile.cuda-ubi9" \
+           --build-arg="TOOL_SERVER_URL=$TOOL_SERVER_URL" \
+           --build-arg="WHEEL_SERVER_URL=$WHEEL_SERVER_URL" \
+           --build-arg="SDIST_SERVER_URL=$SDIST_SERVER_URL" \
+           .
+
+    # Run the image to build the wheel.
+    podman run -it \
+           --name="$image_tag" \
+           --replace \
+           --security-opt label=disable \
+           -e "BUILD_ORDER_FILE=$BUILD_ORDER_FILE" \
+           --volume .:/src:rw,exec \
+           "$image_tag"
+}
+
+inside_of_container() {
+    export CMAKE_ARGS="-DLLAMA_CUBLAS=on"
+    export CFLAGS="-mno-avx"
+
+    jq -r '.[] | .dist + " " + .version' "$BUILD_ORDER_FILE" | while read -r dist version; do
+        ./build_wheel.sh "$dist" "$version"
+        # FIXME: After this loop, artifacts/build-artifacts.tar
+        # includes all of the wheels and sdists, but only the logs for
+        # the last thing built.
+    done
+
+    # Show what all the binary wheels linked to. Using auditwheel on
+    # something that is pure python produces an error, so look for
+    # packages with the arch in them so we only get binary wheels.
+    # shellcheck disable=SC2231
+    for wheel in wheels-repo/downloads/*$(uname -m)*.whl; do
+        auditwheel show "$wheel"
+    done
+}
+
+IN_CONTAINER=${IN_CONTAINER:-false}
+if [ -n "$1" ]; then
+   BUILD_ORDER_FILE="$1"
+else
+    BUILD_ORDER_FILE="${BUILD_ORDER_FILE:-llama-cpp-build-order.json}"
+fi
+
+if "${IN_CONTAINER}"; then
+    inside_of_container
+else
+    outside_of_container
+fi
