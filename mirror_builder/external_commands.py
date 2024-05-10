8448d81ac6ed3a515c87128863b7720c6dcb0ff5
Doug Hellmann <dhellmann@redhat.com>
2024-05-10 13:53:46 -0400

    external_commands: support logging individual commands to files
    Some build commands produce a lot of output, so optionally write to a
    file.

--- mirror_builder/external_commands.py
+++ mirror_builder/external_commands.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 # based on pyproject_hooks/_impl.py: quiet_subprocess_runner
-def run(cmd, cwd=None, extra_environ={}):
+def run(cmd, cwd=None, extra_environ={}, log_filename=None):
     """Call the subprocess while logging output
     """
     env = os.environ.copy()
@@ -18,14 +18,26 @@ def run(cmd, cwd=None, extra_environ={}):
         ' '.join('%s=%s' % x for x in extra_environ.items()),
         ' '.join(shlex.quote(str(s)) for s in cmd)
     )
-    completed = subprocess.run(
-        cmd,
-        cwd=cwd,
-        env=env,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-    )
-    output = completed.stdout.decode('utf-8') if completed.stdout else ''
+    if log_filename:
+        with open(log_filename, 'w') as log_file:
+            completed = subprocess.run(
+                cmd,
+                cwd=cwd,
+                env=env,
+                stdout=log_file,
+                stderr=subprocess.STDOUT,
+            )
+        with open(log_filename, 'r', encoding='utf-8') as f:
+            output = f.read()
+    else:
+        completed = subprocess.run(
+            cmd,
+            cwd=cwd,
+            env=env,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+        output = completed.stdout.decode('utf-8') if completed.stdout else ''
     if completed.returncode != 0:
         logger.error('%s failed with %s', cmd, output)
         raise subprocess.CalledProcessError(completed.returncode, cmd, output)
