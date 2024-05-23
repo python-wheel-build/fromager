b7780b796e9f7e31a7e2af017b8e5bd8dbcc4118
Doug Hellmann <dhellmann@redhat.com>
2024-05-23 17:59:08 -0400

    remove unused code from overrides module

--- mirror_builder/overrides.py
+++ mirror_builder/overrides.py
@@ -1,6 +1,4 @@
-import fnmatch
 import logging
-from importlib import resources
 
 from packaging.utils import canonicalize_name
 from stevedore import extension
@@ -17,33 +15,6 @@ _mgr = extension.ExtensionManager(
 
 
 logger = logging.getLogger(__name__)
-
-
-def _files_for_pkg(anchor, pkg_base, ext):
-    """Iterator producing files to apply to the source dir.
-
-    Input should be the package in which to look for files, the
-    pkg name as a base for matching files, and the file extension.
-
-    Yields pathlib.Path() references to files in the order they
-    are found, which is controlled through lexical sorting of
-    the filenames.
-
-    """
-    # importlib.resources.files gives us back a MultiplexedPath, but
-    # that doesn't support a glob() method directly, so we have to
-    # look through the list of files in the path ourselves.
-    files_dir = resources.files(anchor)
-    pattern = pkg_base + '*' + ext
-    logger.debug('looking in %s for files matching %s', files_dir, pattern)
-    for p in sorted(files_dir.iterdir()):
-        if not fnmatch.fnmatch(p.name, '*' + ext):
-            # ignore things like python files so we don't log excessively
-            continue
-        if not fnmatch.fnmatch(p.name, pattern):
-            logger.debug(f'{p.name} does not match {pattern}')
-            continue
-        yield p
 
 
 def patches_for_source_dir(patches_dir, source_dir_name):
