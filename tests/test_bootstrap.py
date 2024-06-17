dd295f4d606e1a6e850492c8a9aa1a210597052f
Doug Hellmann <dhellmann@redhat.com>
2024-06-17 14:54:54 -0400

    when bootstrapping, report origin of each requirement

--- tests/test_bootstrap.py
+++ tests/test_bootstrap.py
@@ -5,19 +5,19 @@ from fromager.commands import bootstrap
 
 def test_get_requirements_single_arg():
     requirements = bootstrap._get_requirements_from_args(['a'], [])
-    assert ['a'] == requirements
+    assert [('toplevel', 'a')] == requirements
 
 
 def test_get_requirements_multiple_args():
     requirements = bootstrap._get_requirements_from_args(['a', 'b'], [])
-    assert ['a', 'b'] == requirements
+    assert [('toplevel', 'a'), ('toplevel', 'b')] == requirements
 
 
 def test_get_requirements_requirements_file(tmp_path):
     requirements_file = tmp_path / 'requirements.txt'
     requirements_file.write_text('c\n')
     requirements = bootstrap._get_requirements_from_args([], [requirements_file])
-    assert ['c'] == requirements
+    assert [(str(requirements_file), 'c')] == requirements
 
 
 def test_get_requirements_requirements_file_comments(tmp_path):
@@ -31,7 +31,7 @@ def test_get_requirements_requirements_file_comments(tmp_path):
         '''),
     )
     requirements = bootstrap._get_requirements_from_args([], [requirements_file])
-    assert ['c', 'd'] == requirements
+    assert [(str(requirements_file), 'c'), (str(requirements_file), 'd')] == requirements
 
 
 def test_get_requirements_requirements_file_multiple(tmp_path):
@@ -40,18 +40,19 @@ def test_get_requirements_requirements_file_multiple(tmp_path):
     requirements2_file = tmp_path / 'requirements2.txt'
     requirements2_file.write_text('b\n')
     requirements = bootstrap._get_requirements_from_args([], [requirements1_file, requirements2_file])
-    assert ['a', 'b'] == requirements
+    assert [(str(requirements1_file), 'a'), (str(requirements2_file), 'b')] == requirements
 
 
 def test_get_requirements_file_with_comments_and_blanks(tmp_path):
     requirements_file = tmp_path / 'requirements.txt'
     requirements_file.write_text('a\n\n# ignore\nb\nc\n')
     requirements = bootstrap._get_requirements_from_args([], [requirements_file])
-    assert ['a', 'b', 'c'] == requirements
+    f = str(requirements_file)
+    assert [(f, 'a'), (f, 'b'), (f, 'c')] == requirements
 
 
 def test_get_requirements_args_and_file(tmp_path):
     requirements_file = tmp_path / 'requirements.txt'
     requirements_file.write_text('c\n')
     requirements = bootstrap._get_requirements_from_args(['a', 'b'], [requirements_file])
-    assert ['a', 'b', 'c'] == requirements
+    assert [('toplevel', 'a'), ('toplevel', 'b'), (str(requirements_file), 'c')] == requirements
