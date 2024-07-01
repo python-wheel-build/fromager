d2ebdbf470440bd5b71a4c15991e5cd750fe6e88
Doug Hellmann <doug@doughellmann.com>
2024-07-01 19:52:27 -0400

    expand test coverage for fromager.dependencies
    Add some unit tests that pull the dependencies of fromager itself.

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,6 +1,15 @@
-import pytest
+import functools
+import itertools
+import pathlib
+import shutil
+import typing
 
-from fromager import dependencies
+import pytest
+from packaging.requirements import Requirement
+
+from fromager import context, dependencies
+
+_fromager_root = pathlib.Path(__file__).parent.parent
 
 
 @pytest.mark.parametrize(
@@ -32,7 +41,70 @@ from fromager import dependencies
         ),
     ],
 )
-def test_get_build_backend(build_system, expected_results):
+def test_get_build_backend(
+    build_system: dict[str, list[str]] | dict[str, str | list[str] | None],
+    expected_results: dict[str, typing.Any] | dict[str, str | list[str] | None],
+):
     pyproject_toml = {"build-system": build_system}
     actual = dependencies.get_build_backend(pyproject_toml)
     assert expected_results == actual
+
+
+def _clean_build_artifacts(f):
+    @functools.wraps(f)
+    def _with_cleanup(*args, **kwds):
+        try:
+            f(*args, **kwds)
+        finally:
+            for d in itertools.chain(
+                _fromager_root.glob("fromager-*.dist-info"),
+                _fromager_root.glob("fromager.egg-info"),
+            ):
+                shutil.rmtree(d)
+
+
+@_clean_build_artifacts
+def test_get_build_system_dependencies(tmp_context: context.WorkContext):
+    results = dependencies.get_build_system_dependencies(
+        tmp_context,
+        Requirement("fromager"),
+        _fromager_root,
+    )
+    names = set(r.name for r in results)
+    assert names == set(["setuptools", "setuptools_scm"])
+
+
+@_clean_build_artifacts
+def test_get_build_backend_dependencies(tmp_context: context.WorkContext):
+    results = dependencies.get_build_backend_dependencies(
+        tmp_context,
+        Requirement("fromager"),
+        _fromager_root,
+    )
+    names = set(r.name for r in results)
+    assert names == set()
+
+
+@_clean_build_artifacts
+def test_get_build_sdist_dependencies(tmp_context: context.WorkContext):
+    results = dependencies.get_build_sdist_dependencies(
+        tmp_context,
+        Requirement("fromager"),
+        _fromager_root,
+    )
+    names = set(r.name for r in results)
+    assert names == set()
+
+
+@_clean_build_artifacts
+def test_get_install_dependencies(tmp_context: context.WorkContext):
+    pyproject_contents = dependencies.get_pyproject_contents(_fromager_root)
+    expected = set(
+        Requirement(d) for d in pyproject_contents["project"]["dependencies"]
+    )
+    actual = dependencies.get_install_dependencies(
+        tmp_context,
+        Requirement("fromager"),
+        _fromager_root,
+    )
+    assert actual == expected
