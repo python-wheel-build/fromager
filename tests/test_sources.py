ad3762aafa8a264e2d73b34fc2a21300778aa24c
Rohan Devasthale <rdevasth@redhat.com>
2024-07-09 16:26:45 -0400

    Passing a fake url as parameter to the function

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -9,9 +9,10 @@ from fromager import sources
 @patch("fromager.sources.download_url")
 def test_invalid_tarfile(mock_download_url, tmp_path: pathlib.Path):
     mock_download_url.return_value = pathlib.Path(tmp_path / "test" / "fake_wheel.txt")
+    fake_url = "https://www.thisisafakeurl.com"
     fake_dir = tmp_path / "test"
     fake_dir.mkdir()
     text_file = fake_dir / "fake_wheel.txt"
     text_file.write_text("This is a test file")
     with pytest.raises(TypeError):
-        sources._download_source_check(fake_dir, text_file)
+        sources._download_source_check(fake_dir, fake_url)
