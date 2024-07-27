f4f38cd8c41aa04dcc06dcc4141beb3da5f39286
Christian Heimes <cheimes@redhat.com>
2024-07-27 10:27:49 +0200

    Use Pydantic to validate and manage settings
    Pydantic provides a convenient way to validate data and transform it
    into type-safe Python objects with defaults.
    
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,6 +1,8 @@
 import textwrap
 
-from fromager import settings
+from pydantic import AnyUrl
+
+from fromager import settings, settings_model
 
 
 def test_empty():
@@ -86,3 +88,84 @@ def test_no_resolver_dist():
     assert s.resolver_include_sdists("foo") is None
     assert s.resolver_include_wheels("foo") is None
     assert s.resolver_sdist_server_url("foo") is None
+
+
+def test_full():
+    raw = textwrap.dedent("""
+        pre_built:
+            camelot:
+                - egg
+                - spam
+            knight:
+        packages:
+            egg:
+                download_source:
+                    url: https://egg.test
+                    destination_filename: new_filename
+            resolver_dist:
+                sdist_server_url: https://sdist.test/egg
+                include_sdists: true
+                include_wheels: false
+            spam: {}
+            islington:
+                download_source:
+                resolver_dist:
+    """)
+    s = settings_model.parse_settings(raw)
+    assert s.model_dump(mode="python") == {
+        "packages": {
+            "egg": {
+                "download_source": {
+                    "destination_filename": "new_filename",
+                    "url": AnyUrl("https://egg.test/"),
+                },
+                "resolver_dist": {
+                    "include_sdists": True,
+                    "include_wheels": False,
+                    "sdist_server_url": None,
+                },
+            },
+            "islington": {
+                "download_source": {"destination_filename": None, "url": None},
+                "resolver_dist": {
+                    "include_sdists": True,
+                    "include_wheels": False,
+                    "sdist_server_url": None,
+                },
+            },
+            "resolver-dist": {
+                "download_source": {"destination_filename": None, "url": None},
+                "resolver_dist": {
+                    "include_sdists": True,
+                    "include_wheels": False,
+                    "sdist_server_url": None,
+                },
+            },
+            "spam": {
+                "download_source": {"destination_filename": None, "url": None},
+                "resolver_dist": {
+                    "include_sdists": True,
+                    "include_wheels": False,
+                    "sdist_server_url": None,
+                },
+            },
+        },
+        "pre_built": {"camelot": {"spam", "egg"}, "knight": set()},
+    }
+    info = s.get_package("egg", variant="camelot")
+    assert info.model_dump(mode="python") == {
+        "name": "egg",
+        "pre_built": True,
+        "settings": {
+            "download_source": {
+                "destination_filename": "new_filename",
+                "url": AnyUrl("https://egg.test/"),
+            },
+            "resolver_dist": {
+                "include_sdists": True,
+                "include_wheels": False,
+                "sdist_server_url": None,
+            },
+        },
+        "variant": "camelot",
+    }
