7a4168633a5163b2cdbc6760a019c299131b598a
Shubh Bapna <sbapna@redhat.com>
2024-09-13 15:16:26 -0400

    create all directories on setup

--- e2e/test_elfdeps.sh
+++ e2e/test_elfdeps.sh
@@ -14,7 +14,7 @@ VERSION="2.1.5"
 OS=$(uname)
 if [ "$OS" = "Darwin" ]; then
     NETWORK_ISOLATION=""
-    WHEEL_PLATFORM_TAG="macosx_10_9_universal2"
+    WHEEL_PLATFORM_TAG="macosx_10_13_universal2"
     HAS_ELFDEP="0"
 else
     NETWORK_ISOLATION="--network-isolation"
