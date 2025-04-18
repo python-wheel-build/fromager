7f04e127e0aa1d03af4c32a2542f89a504cf39b5
Doug Hellmann <dhellmann@redhat.com>
2025-04-18 17:46:18 -0400

    let bootstrapper reuse a wheel on the local filesystem
    Update the bootstrapper to reuse wheels already present on the local
    filesystem in the work directory instead of only going to the cache
    server. This ensures that someone working on multiple related wheels can
    reuse one expensive one when building the next.
    
    Update the test for caching in the bootstrap logic to focus on the cases
    of an existing wheel on the filesystem and a wheel available from any
    cache server. This removes the separate test for using a local cache
    server versus using a remote one, since there should effectively be no
    difference in those cases.

--- e2e/test_bootstrap_cache.sh
+++ e2e/test_bootstrap_cache.sh
@@ -7,72 +7,78 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPTDIR/common.sh"
 pass=true
 
+DIST=setuptools
+VER=78.1.0
+
+################################################################################
 # run fromager once to build wheels that can be used by a local wheel server
+
 fromager \
+  --debug \
   --log-file="$OUTDIR/bootstrap.log" \
   --error-log-file="$OUTDIR/fromager-errors.log" \
   --sdists-repo="$OUTDIR/sdists-repo" \
   --wheels-repo="$OUTDIR/wheels-repo" \
   --work-dir="$OUTDIR/work-dir" \
   --settings-dir="$SCRIPTDIR/changelog_settings" \
-  bootstrap 'setuptools'
+  bootstrap "$DIST==$VER"
 
-start_local_wheel_server
-rm -rf "$OUTDIR/sdists-repo"
+################################################################################
+# run fromager again to verify that we pick up the existing wheels in the output directory
+
+# Remove build work-dir and wheel server dir, but not the sdist or wheel repos
 rm -rf "$OUTDIR/work-dir"
+rm -rf "$OUTDIR/wheels-repo/simple"
 rm "$OUTDIR/bootstrap.log"
 
-
-# run fromager with the cache wheel server pointing to the local wheel server
 fromager \
+  --debug \
   --log-file="$OUTDIR/bootstrap.log" \
   --error-log-file="$OUTDIR/fromager-errors.log" \
   --sdists-repo="$OUTDIR/sdists-repo" \
   --wheels-repo="$OUTDIR/wheels-repo" \
   --work-dir="$OUTDIR/work-dir" \
   --settings-dir="$SCRIPTDIR/changelog_settings" \
-  --no-cleanup \
-  bootstrap --cache-wheel-server-url=$WHEEL_SERVER_URL 'setuptools'
+  bootstrap --cache-wheel-server-url="https://pypi.org/simple" "$DIST==$VER"
 
 EXPECTED_LOG_MESSAGES=(
-"setuptools: loading build sdist dependencies from build-sdist-requirements.txt"
-"setuptools: loading build backend dependencies from build-backend-requirements.txt"
-"setuptools: loading build system dependencies from build-system-requirements.txt"
+"$DIST: looking for existing wheel for version $VER with build tag () in"
+"$DIST: found existing wheel"
 )
-
 for pattern in "${EXPECTED_LOG_MESSAGES[@]}"; do
-  echo $pattern
   if ! grep -q "$pattern" "$OUTDIR/bootstrap.log"; then
     echo "FAIL: Did not find log message $pattern in $OUTDIR/bootstrap.log" 1>&2
     pass=false
   fi
 done
+$pass
 
-EXPECTED_FILES="
-$OUTDIR/wheels-repo/downloads/setuptools-*.whl
+UNEXPECTED_LOG_MESSAGES=(
+"$DIST: checking if wheel was already uploaded to https://pypi.org/simple"
+)
 
-$OUTDIR/work-dir/setuptools-*/*-requirements.txt
-
-$OUTDIR/work-dir/build-order.json
-$OUTDIR/work-dir/constraints.txt
-"
-
-for pattern in $EXPECTED_FILES; do
-  if [ ! -f "${pattern}" ]; then
-    echo "Did not find $pattern" 1>&2
+for pattern in "${UNEXPECTED_LOG_MESSAGES[@]}"; do
+  if grep -q "$pattern" "$OUTDIR/bootstrap.log"; then
+    echo "FAIL: Found log message $pattern in $OUTDIR/bootstrap.log" 1>&2
     pass=false
   fi
 done
-
 $pass
 
+################################################################################
+
+# run fromager with the cache wheel server pointing to the pypi server and
+# verify we can pick it up from there
+
+start_local_wheel_server
 rm -rf "$OUTDIR/sdists-repo"
 rm -rf "$OUTDIR/work-dir"
-rm -rf "$OUTDIR/wheels-repo"
+rm -rf "$OUTDIR/wheels-repo/build"
+rm -rf "$OUTDIR/wheels-repo/download"
 rm "$OUTDIR/bootstrap.log"
 
-# run fromager with the cache wheel server pointing to the pypi server
 fromager \
+  --debug \
   --log-file="$OUTDIR/bootstrap.log" \
   --error-log-file="$OUTDIR/fromager-errors.log" \
   --sdists-repo="$OUTDIR/sdists-repo" \
@@ -80,7 +86,7 @@ fromager \
   --work-dir="$OUTDIR/work-dir" \
   --settings-dir="$SCRIPTDIR/changelog_settings" \
   --no-cleanup \
-  bootstrap --cache-wheel-server-url="https://pypi.org/simple" 'setuptools'
+  bootstrap --cache-wheel-server-url="https://pypi.org/simple" "$DIST==$VER"
 
 EXPECTED_FILES="
 $OUTDIR/wheels-repo/downloads/setuptools-*.whl
@@ -98,7 +104,13 @@ done
 
 $pass
 
-for pattern in "${EXPECTED_LOG_MESSAGES[@]}"; do
+UNEXPECTED_LOG_MESSAGES=(
+"$DIST: loading build sdist dependencies from build-sdist-requirements.txt"
+"$DIST: loading build backend dependencies from build-backend-requirements.txt"
+"$DIST: loading build system dependencies from build-system-requirements.txt"
+)
+
+for pattern in "${UNEXPECTED_LOG_MESSAGES[@]}"; do
   echo $pattern
   if grep -q "$pattern" "$OUTDIR/bootstrap.log"; then
     echo "FAIL: found log message $pattern in $OUTDIR/bootstrap.log" 1>&2
