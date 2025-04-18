14ef7b83672eda428a2874f25b9b35ebbbd269d6
Doug Hellmann <dhellmann@redhat.com>
2025-04-18 17:46:18 -0400

    let bootstrapper reuse a wheel on the local filesystem
    Update the bootstrapper to reuse wheels already present on the local
    filesystem in the work directory instead of only going to the cache
    server. This ensures that someone working on multiple related wheels can
    reuse one expensive one when building the next.
    
    Update the test for caching in the bootstrap logic to focus on the cases
    of an existing wheel on the filesystem and a wheel available from any
    cache server. This removes the separate test for using a local cache
    server versus using a remote one, since there should effectively be no
    difference in those cases.

--- e2e/test_bootstrap_build_tags.sh
+++ e2e/test_bootstrap_build_tags.sh
@@ -8,7 +8,7 @@ source "$SCRIPTDIR/common.sh"
 
 # bootstrap stevedore with 1 change log
 fromager \
-  --log-file="$OUTDIR/bootstrap.log" \
+  --log-file="$OUTDIR/bootstrap1.log" \
   --error-log-file="$OUTDIR/fromager-errors.log" \
   --sdists-repo="$OUTDIR/sdists-repo" \
   --wheels-repo="$OUTDIR/wheels-repo" \
@@ -20,8 +20,6 @@ EXPECTED_FILES="
 $OUTDIR/wheels-repo/downloads/stevedore-5.2.0-1*.whl
 "
 
-start_local_wheel_server
-
 pass=true
 for pattern in $EXPECTED_FILES; do
   if [ ! -f "${pattern}" ]; then
@@ -32,9 +30,24 @@ done
 
 $pass
 
-# bootstrap stevedore again with 1 change log. Should not result in a build
+# bootstrap stevedore again with 1 change log without a file in the downloads
+# cache but with a file in the cache package server. Should not result in a
+# build because the file exists in the cache server directory.
+
+find "$OUTDIR/wheels-repo"
+# replace the symlink in the index dir with an actual file so it can be
+# downloaded, and remove the wheels from the disk cache so we know we get the
+# file from the web server
+rm -f $OUTDIR/wheels-repo/simple/stevedore/*.whl
+mv $OUTDIR/wheels-repo/downloads/stevedore*.whl $OUTDIR/wheels-repo/simple/stevedore/
+rm -f $OUTDIR/wheels-repo/build/*.whl
+rm -f $OUTDIR/wheels-repo/downloads/*.whl
+find "$OUTDIR/wheels-repo"
+start_local_wheel_server
+
+LOGFILE="$OUTDIR/bootstrap2.log"
 fromager \
-  --log-file="$OUTDIR/bootstrap.log" \
+  --log-file="$LOGFILE" \
   --error-log-file="$OUTDIR/fromager-errors.log" \
   --sdists-repo="$OUTDIR/sdists-repo" \
   --wheels-repo="$OUTDIR/wheels-repo" \
@@ -42,16 +55,39 @@ fromager \
   --settings-file="$SCRIPTDIR/bootstrap_settings.yaml" \
   bootstrap --cache-wheel-server-url=$WHEEL_SERVER_URL 'stevedore==5.2.0'
 
-if ! grep -q "stevedore: found built wheel on cache server" "$OUTDIR/bootstrap.log"; then
-  echo "FAIL: Did not find log message found built wheel on cache server in $OUTDIR/bootstrap.log" 1>&2
+if ! grep -q "stevedore: found built wheel on cache server" "$LOGFILE"; then
+  echo "FAIL: Did not find log message found built wheel on cache server in $LOGFILE" 1>&2
+  pass=false
+fi
+
+$pass
+
+# bootstrap stevedore again with 1 change log. Should not result in a build
+# because the file exists in the cache server directory.
+
+find "$OUTDIR/wheels-repo"
+
+LOGFILE="$OUTDIR/bootstrap3.log"
+fromager \
+  --log-file="$LOGFILE" \
+  --error-log-file="$OUTDIR/fromager-errors.log" \
+  --sdists-repo="$OUTDIR/sdists-repo" \
+  --wheels-repo="$OUTDIR/wheels-repo" \
+  --work-dir="$OUTDIR/work-dir" \
+  --settings-file="$SCRIPTDIR/bootstrap_settings.yaml" \
+  bootstrap --cache-wheel-server-url=$WHEEL_SERVER_URL 'stevedore==5.2.0'
+
+if ! grep -q "stevedore: found existing wheel " "$LOGFILE"; then
+  echo "FAIL: Did not find log message found existing wheel in $LOGFILE" 1>&2
   pass=false
 fi
 
 $pass
 
 # bootstrap stevedore with 2 changelog. should result in a build instead of being skipped
+LOGFILE="$OUTDIR/bootstrap4.log"
 fromager \
-  --log-file="$OUTDIR/bootstrap_build_tags.log" \
+  --log-file="$LOGFILE" \
   --error-log-file="$OUTDIR/fromager-errors.log" \
   --sdists-repo="$OUTDIR/sdists-repo" \
   --wheels-repo="$OUTDIR/wheels-repo" \
@@ -72,8 +108,8 @@ for pattern in $EXPECTED_FILES; do
   fi
 done
 
-if ! grep -q "added extra metadata and build tag" "$OUTDIR/bootstrap_build_tags.log"; then
-  echo "Did not find message indicating builds would be skipped" 1>&2
+if ! grep -q "added extra metadata and build tag" "$LOGFILE"; then
+  echo "Did not find message indicating builds would be skipped in $LOGFILE" 1>&2
   pass=false
 fi
 
