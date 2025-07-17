0fe263166c3429e52718bdd52a0b63e66ecda436
Lukas Brabec <lbrabec@redhat.com>
2025-07-17 22:53:31 +0200

    linter: check for duplicates, check for optional deps in constraints
    Add lint-requirements check for duplicates in in requirements and
    constraints files and check that constraints don't contain optional
    dependencies.
    
    Fixes #546
    Fixes #547
    
    Signed-off-by: Lukas Brabec <lbrabec@redhat.com>

--- src/fromager/commands/lint_requirements.py
+++ src/fromager/commands/lint_requirements.py
@@ -1,4 +1,5 @@
 import logging
+import pathlib
 import sys
 
 import click
@@ -11,13 +12,15 @@ logger = logging.getLogger(__name__)
 
 @click.command()
 @click.argument(
-    "input_files_path", nargs=-1, required=True, type=click.Path(exists=False)
+    "input_files_path", nargs=-1, required=True, type=click.Path(exists=False, path_type=pathlib.Path)
 )
-def lint_requirements(input_files_path: list[click.Path]) -> None:
+def lint_requirements(input_files_path: list[pathlib.Path]) -> None:
     """
     Command to lint the constraints.txt and requirements.txt files
     This command takes a single wildcard path string for constraints.txt and requirements.txt.
-    It checks the formatting of these files and reports issues if found.
+    It checks the formatting of these files and reports issues if found. Files with names that
+    end with constraints.txt (e.g. constraints.txt, global-constraints.txt, etc.) are not allowed
+    to contain extra dependencies.
     """
 
     if len(input_files_path) == 0:
@@ -27,10 +30,16 @@ def lint_requirements(input_files_path: list[click.Path]) -> None:
     flag = True
 
     for path in input_files_path:
-        parsed_lines = requirements_file.parse_requirements_file(str(path))
+        parsed_lines = requirements_file.parse_requirements_file(path)
+        unique_entries: dict[str, Requirement] = {}
         for line in parsed_lines:
             try:
-                Requirement(line)
+                requirement = Requirement(line)
+                if requirement.name in unique_entries:
+                    raise InvalidRequirement(f"Duplicate entry, first found: {unique_entries[requirement.name]}")
+                unique_entries[requirement.name] = requirement
+                if requirement.extras and path.name.endswith("constraints.txt"):
+                    raise InvalidRequirement("Constraints files cannot contain extra dependencies")
             except InvalidRequirement as err:
                 logger.error(f"{path}: {line}: {err}")
                 flag = False
