40bbeb109a8aae60678a6b7f31ae6484fa685698
Christian Heimes <cheimes@redhat.com>
2025-10-25 09:13:50 +0200

    feat: use pypi_simple for PyPI simple API
    The project https://pypi.org/project/pypi-simple/ provides an
    easy-to-use, convenient wrapper around PyPI's simple HTML and JSON API
    as well as a bunch of packaging PEPs. Replace our custom `html5lib` and
    `requests` library with pypi-simple.
    
    PyPI API calls now prefer the faster, more feature-rich JSON API and
    fall back to HTML for local index and GitLab.
    
    Fixes: #741
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -62,7 +62,7 @@ def test_provider_choose_wheel():
         candidate = result.mapping["hydra-core"]
         assert (
             candidate.url
-            == "https://files.pythonhosted.org/packages/c6/50/e0edd38dcd63fb26a8547f13d28f7a008bc4a3fd4eb4ff030673f22ad41a/hydra_core-1.3.2-2-py3-none-any.whl#sha256=fa0238a9e31df3373b35b0bfb672c34cc92718d21f81311d8996a16de1141d8b"
+            == "https://files.pythonhosted.org/packages/c6/50/e0edd38dcd63fb26a8547f13d28f7a008bc4a3fd4eb4ff030673f22ad41a/hydra_core-1.3.2-2-py3-none-any.whl"
         )
         assert str(candidate.version) == "1.3.2"
 
@@ -196,7 +196,7 @@ def test_provider_choose_sdist():
         candidate = result.mapping["hydra-core"]
         assert (
             candidate.url
-            == "https://files.pythonhosted.org/packages/6d/8e/07e42bc434a847154083b315779b0a81d567154504624e181caf2c71cd98/hydra-core-1.3.2.tar.gz#sha256=8a878ed67216997c3e9d88a8e72e7b4767e81af37afb4ea3334b269a4390a824"
+            == "https://files.pythonhosted.org/packages/6d/8e/07e42bc434a847154083b315779b0a81d567154504624e181caf2c71cd98/hydra-core-1.3.2.tar.gz"
         )
         assert str(candidate.version) == "1.3.2"
 
@@ -222,9 +222,9 @@ def test_provider_choose_either_with_constraint():
         candidate = result.mapping["hydra-core"]
         assert (
             candidate.url
-            == "https://files.pythonhosted.org/packages/6d/8e/07e42bc434a847154083b315779b0a81d567154504624e181caf2c71cd98/hydra-core-1.3.2.tar.gz#sha256=8a878ed67216997c3e9d88a8e72e7b4767e81af37afb4ea3334b269a4390a824"
+            == "https://files.pythonhosted.org/packages/6d/8e/07e42bc434a847154083b315779b0a81d567154504624e181caf2c71cd98/hydra-core-1.3.2.tar.gz"
             or candidate.url
-            == "https://files.pythonhosted.org/packages/c6/50/e0edd38dcd63fb26a8547f13d28f7a008bc4a3fd4eb4ff030673f22ad41a/hydra_core-1.3.2-2-py3-none-any.whl#sha256=fa0238a9e31df3373b35b0bfb672c34cc92718d21f81311d8996a16de1141d8b"
+            == "https://files.pythonhosted.org/packages/c6/50/e0edd38dcd63fb26a8547f13d28f7a008bc4a3fd4eb4ff030673f22ad41a/hydra_core-1.3.2-2-py3-none-any.whl"
         )
 
 
@@ -264,7 +264,7 @@ def test_provider_constraint_match():
         candidate = result.mapping["hydra-core"]
         assert (
             candidate.url
-            == "https://files.pythonhosted.org/packages/6d/8e/07e42bc434a847154083b315779b0a81d567154504624e181caf2c71cd98/hydra-core-1.2.2.tar.gz#sha256=8a878ed67216997c3e9d88a8e72e7b4767e81af37afb4ea3334b269a4390a824"
+            == "https://files.pythonhosted.org/packages/6d/8e/07e42bc434a847154083b315779b0a81d567154504624e181caf2c71cd98/hydra-core-1.2.2.tar.gz"
         )
         assert str(candidate.version) == "1.2.2"
 
